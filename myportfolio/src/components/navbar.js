e944a5da06e918322362b6b0ecf04f26e616b7cc
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2023-11-29 23:05:35 +0530

    Navbar updated

---
+++ myportfolio/src/components/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './navbar.css';
+import logo from '../assets/code.png';
+import contactImg from '../assets/contact.png';
+import {Link} from 'react-scroll';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='navbar'>
+        <img src={logo} alt='Logo'className='logo'/>
+        <div className='desktopMenu'>
+            <Link className='desktopMenuListItem'>Home</Link>
+            <Link className='desktopMenuListItem'>About</Link>
+            <Link className='desktopMenuListItem'>Portfolio</Link>
+            <Link className='desktopMenuListItem'>Clients</Link>
+
+        </div>
+
+        <button className='desktopMenuBtn'>
+            <img src={contactImg} alt='ContactImg' className='desktopMenuImg'/>
+            Contact Me!
+        </button>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
